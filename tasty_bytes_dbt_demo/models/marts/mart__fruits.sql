ad04df721996f9386a753235507ce839e36acb30
Ricardo Arturo Ibarra Casillas <65570739+ribarra96@users.noreply.github.com>
2025-10-07 23:47:27 -0600

    Create mart__fruits.sql

---
+++ tasty_bytes_dbt_demo/models/marts/mart__fruits.sql
@@ -0,0 +1,12 @@
+with src as (
+  
+  select *
+  from {{ref('stg__fruits')}}
+
+)
+
+select *
+from src
+{% if is_incremental() %}
+  where fruit_name not in (select fruit_name from {{ this }})
+{% endif %}
