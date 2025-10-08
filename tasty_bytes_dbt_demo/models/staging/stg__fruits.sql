3b4647bcf23eb4503ddb5de770e88957d98f8f0a
Ricardo Arturo Ibarra Casillas <65570739+ribarra96@users.noreply.github.com>
2025-10-07 23:45:01 -0600

    Update stg__fruits.sql

--- tasty_bytes_dbt_demo/models/staging/stg__fruits.sql
+++ tasty_bytes_dbt_demo/models/staging/stg__fruits.sql
@@ -1,6 +1,3 @@
--- models/dim_fruits.sql
-{{ config(materialized='incremental', unique_key='fruit_name', on_schema_change='sync') }}
-
 with src as (
   select payload from {{ source('onepiece_staging','STG_ONEPIECE_FRUITS') }}
 ),
@@ -12,6 +9,4 @@ flat as (
   from src
 )
 select * from flat
-{% if is_incremental() %}
-where fruit_name not in (select fruit_name from {{ this }})
-{% endif %}
+
