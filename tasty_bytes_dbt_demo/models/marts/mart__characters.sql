6d2cb9ef0a5510a183eb21e0742bbe5cdb70200f
Ricardo Arturo Ibarra Casillas <65570739+ribarra96@users.noreply.github.com>
2025-10-07 23:48:45 -0600

    Create mart__characters.sql

---
+++ tasty_bytes_dbt_demo/models/marts/mart__characters.sql
@@ -0,0 +1,9 @@
+with src as (
+  select *
+  from {{ref('stg__characters')}}
+)
+select *
+from src 
+{% if is_incremental() %}
+  where character_name not in (select character_name from {{ this }})
+{% endif %}
